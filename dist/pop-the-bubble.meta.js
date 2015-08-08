58d7fabb5aa13a8314bcbd89f85e4a900400a673
Asbjørn Apeland <aude@users.noreply.github.com>
2015-08-08 12:22:58 +0200

    better versioning

--- dist/pop-the-bubble.meta.js
+++ dist/pop-the-bubble.meta.js
@@ -1,33 +1,33 @@
 // ==UserScript==
-// @name        Pop the Bubble
-// @namespace   https://github.com/aude
-// @author      aude
-// @description News sites are great, but they try too hard to keep you on the site. This script highlights links to external pages in blue, so you can pull them out of articles easier.
-// @include     /^https?://(www\.)?phoronix\.com/.*/
-// @include     /^https?://(www\.)?lifehacker\.com/.*/
-// @include     /^https?://(www\.)?arstechnica\.(com|co\.uk)/.*/
-// @include     /^https?://blog\.codinghorror\.com/.*/
-// @include     /^https?://(www\.)?tek\.no/.*/
-// @domain      phoronix.com
-// @domain      lifehacker.com
-// @domain      arstechnica.com
-// @domain      arstechnica.co.uk
-// @domain      blog.codinghorror.com
-// @domain      tek.no
-// @match       http://phoronix.com/*
-// @match       https://phoronix.com/*
-// @match       http://lifehacker.com/*
-// @match       https://lifehacker.com/*
-// @match       http://arstechnica.com/*
-// @match       https://arstechnica.com/*
-// @match       http://arstechnica.co.uk/*
-// @match       https://arstechnica.co.uk/*
-// @match       http://blog.codinghorror.com/*
-// @match       https://blog.codinghorror.com/*
-// @match       http://tek.no/*
-// @match       https://tek.no/*
-// @updateURL   https://github.com/aude/pop-the-bubble.user.js/raw/master/dist/pop-the-bubble.meta.js
+// @name	Pop the Bubble
+// @namespace	https://github.com/aude
+// @author	aude
+// @description	News sites are great, but they try too hard to keep you on the site. This script highlights links to external pages in blue, so you can pull them out of articles easier.
+// @include	/^https?://(www\.)?phoronix\.com/.*/
+// @include	/^https?://(www\.)?lifehacker\.com/.*/
+// @include	/^https?://(www\.)?arstechnica\.(com|co\.uk)/.*/
+// @include	/^https?://blog\.codinghorror\.com/.*/
+// @include	/^https?://(www\.)?tek\.no/.*/
+// @domain	phoronix.com
+// @domain	lifehacker.com
+// @domain	arstechnica.com
+// @domain	arstechnica.co.uk
+// @domain	blog.codinghorror.com
+// @domain	tek.no
+// @match	http://phoronix.com/*
+// @match	https://phoronix.com/*
+// @match	http://lifehacker.com/*
+// @match	https://lifehacker.com/*
+// @match	http://arstechnica.com/*
+// @match	https://arstechnica.com/*
+// @match	http://arstechnica.co.uk/*
+// @match	https://arstechnica.co.uk/*
+// @match	http://blog.codinghorror.com/*
+// @match	https://blog.codinghorror.com/*
+// @match	http://tek.no/*
+// @match	https://tek.no/*
+// @updateURL	https://github.com/aude/pop-the-bubble.user.js/raw/master/dist/pop-the-bubble.meta.js
 // @downloadURL https://github.com/aude/pop-the-bubble.user.js/raw/master/dist/pop-the-bubble.user.js
-// @grant       none
-// @version     150807
+// @grant	none
+// @version	20150807
 // ==/UserScript==
